07f05ba0a0496aebfa9b46ce7653445bcd210fe3
Raphael <rparodi@student.42.fr>
2025-01-14 23:36:14 +0100

    fix: fixing the telescope plugins

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -3,42 +3,42 @@
 		enable = true;
 
 		settings.defaults = {
-			file_ignore_patterns = [
-				"^.d"
-				"^.git"
-				"^.o"
-				"a.out"
-				"node_modules"
-				"object"
-				"objects"
-			];
-		};
+ 			file_ignore_patterns = [
+				"$.o"
+ 				"$.d"
+ 				"$.git"
+ 				"a.out"
+ 				"node_modules"
+ 				"object"
+ 				"objects"
+ 			];
+ 		};
+ 
+ 		extensions = {
+ 			fzf-native.enable = true;
+ 		};
+ 	};
 
-#		extensions = {
-#			fzf-native.enable = true;
-#		};
-	};
-
-	keymaps = [
-		{
-			mode = "n";
-			key = "<leader>f";
-			action = "<cmd>Telescope find_files prompt_prefix=📂:<cr>";
-		}
-		{
-			mode = "n";
-			key = "<leader>b";
-			action = "<cmd>Telescope buffers prompt_prefix=🔖:<cr>";
-		}
-		{
-			mode = "n";
-			key = "<leader>g";
-			action = "<cmd>Telescope live_grep prompt_prefix=🪄:<cr>";
-		}
-		{
-			mode = "n";
-			key = "<leader>T";
-			action = "<cmd>Telescope<cr>";
-		}
-	];
+ 	keymaps = [
+ 		{
+ 			mode = "n";
+ 			key = "<leader>f";
+ 			action = "<cmd>Telescope find_files prompt_prefix=📂:<cr>";
+ 		}
+ 		{
+ 			mode = "n";
+ 			key = "<leader>b";
+ 			action = "<cmd>Telescope buffers prompt_prefix=🔖:<cr>";
+ 		}
+ 		{
+ 			mode = "n";
+ 			key = "<leader>g";
+ 			action = "<cmd>Telescope live_grep prompt_prefix=🪄:<cr>";
+ 		}
+ 		{
+ 			mode = "n";
+ 			key = "<leader>T";
+ 			action = "<cmd>Telescope<cr>";
+ 		}
+ 	];
 }
