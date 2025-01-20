f3a97bc933c85a446b1b2b926095a4d50b9ccaaa
Raphael <rparodi@student.42.fr>
2025-01-20 23:08:47 +0100

    feat: adding the 42 header

--- config/default.nix
+++ config/default.nix
@@ -1,3 +1,5 @@
+{pkgs, ...}:
+
 {
 	imports = [
 		./plugins/autopair.nix
@@ -19,5 +21,16 @@
 		./settings/command.nix
 		./settings/general.nix
 		./settings/keys.nix
+	];
+	extraPlugins = [
+		(pkgs.vimUtils.buildVimPlugin {
+			pname = "42header";
+			version = "latest";
+			src = pkgs.fetchgit {
+				url = "https://github.com/42Paris/42header.git";
+				rev = "9f51db5cc483951e257aece66f1f4ce3b14cdab1";
+				sha256 = "T4BdswmjlrR3KG+97mzncuJ/1OAvx7GDwXW6MI5fBNE=";
+			};
+		})
 	];
 }
