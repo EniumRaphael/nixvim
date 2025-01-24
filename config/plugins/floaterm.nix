50e557e9fbbb5b36311310d69bdb8e75f29efe54
Raphael <rparodi@student.42.fr>
2025-01-24 17:30:24 +0100

    style: changing the keymaps for floaterm

--- config/plugins/floaterm.nix
+++ config/plugins/floaterm.nix
@@ -3,10 +3,10 @@
 		enable = true;
 		settings = {
 			height = 0.9;
-			keymap_kill = "<Leader>fk";
-			keymap_new = "<Leader>ft";
-			keymap_next = "<Leader>fn";
-			keymap_prev = "<Leader>fp";
+			keymap_kill = "<Leader>Tk";
+			keymap_new = "<Leader>Tt";
+			keymap_next = "<Leader>Tn";
+			keymap_prev = "<Leader>Tp";
 			keymap_toggle = "<Leader>t";
 			opener = "edit ";
 			rootmarkers = [
