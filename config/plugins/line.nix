4cb5f3971e887ecf62c2947c5bcb6dee1aba27e5
Raphael <rparodi@student.42.fr>
2025-10-13 13:58:43 +0200

    feat(plugins/line): adding the line with the catppuccn theme

--- config/plugins/line.nix
+++ config/plugins/line.nix
@@ -1,5 +1,45 @@
 {
   plugins.lualine = {
     enable = true;
+    settings = {
+      options = {
+        theme = "catppuccin";
+        icons_enabled = true;
+        globalstatus = true;
+        section_separators = {
+          left = "";
+          right = "";
+        };
+        component_separators = {
+          left = "";
+          right = "";
+        };
+      };
+
+      sections = {
+        lualine_a = [
+          "mode"
+        ];
+        lualine_b = [
+          "branch"
+          "diff"
+          "diagnostics"
+        ];
+        lualine_c = [
+          "filename"
+        ];
+        lualine_x = [
+          "encoding"
+          "fileformat"
+          "filetype"
+        ];
+        lualine_y = [
+          "progress"
+        ];
+        lualine_z = [
+          "location"
+        ];
+      };
+    };
   };
 }
