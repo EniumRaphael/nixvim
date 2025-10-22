6c5e00957ddf29986465b6eba366684d7cde563d
Raphael <rparodi@student.42.fr>
2025-10-22 02:29:15 +0200

    feat(lsp/servers): adding deep configuration to lsp servers

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -4,12 +4,135 @@
       enable = true;
       inlayHints = true;
       servers = {
-        arduino_language_server.enable = true;
-        asm_lsp.enable = true;
-        clangd.enable = true;
-        eslint.enable = true;
-        nixd.enable = true;
-        ts_ls.enable = true;
+        arduino_language_server = {
+          enable = true;
+          cmd = [
+            "arduino-language-server"
+            "-cli"
+            "arduino-cli"
+            "-clangd"
+            "clangd"
+          ];
+        };
+        asm_lsp = {
+          enable = true;
+          filetypes = [
+            "asm"
+            "s"
+            "S"
+          ];
+        };
+        clangd = {
+          enable = true;
+          cmd = [
+            "clangd"
+            "--background-index"
+            "--clang-tidy"
+            "--cross-file-rename"
+            "--completion-style=detailed"
+            "--header-insertion=iwyu"
+          ];
+          filetypes = [
+            "c"
+            "cpp"
+            "tpp"
+          ];
+        };
+        nixd = {
+          enable = true;
+          settings.nixd = {
+            formatting.command = [
+              "nixfmt"
+            ];
+            options = {
+              nixpkgs.expr = "import <nixpkgs> {}";
+              nixos.expr = "(import <nixpkgs/nixos> { }).config";
+            };
+          };
+        };
+        rust_analyzer = {
+          enable = true;
+          installRustc = true;
+          installCargo = true;
+          settings = {
+            check.command = "clippy";
+            cargo = {
+              allFeatures = true;
+              buildScripts.enable = true;
+            };
+            procMacro.enable = true;
+            imports = {
+              granularity.group = "module";
+              prefix = "self";
+            };
+            inlayHints = {
+              lifetimeElisionHints.enable = "always";
+              typeHints.enable = true;
+              parameterHints.enable = true;
+              chainingHints.enable = true;
+              closureReturnTypeHints.enable = "always";
+            };
+          };
+        };
+        vtsls = {
+          enable = true;
+          settings = {
+            typescript.inlayHints = {
+              parameterNames.enabled = "all";
+              parameterTypes.enabled = true;
+              variableTypes.enabled = true;
+              propertyDeclarationTypes.enabled = true;
+              functionLikeReturnTypes.enabled = true;
+              enumMemberValues.enabled = true;
+            };
+            javascript.inlayHints = {
+              parameterNames.enabled = "all";
+              parameterTypes.enabled = true;
+              variableTypes.enabled = true;
+              propertyDeclarationTypes.enabled = true;
+              functionLikeReturnTypes.enabled = true;
+              enumMemberValues.enabled = true;
+            };
+          };
+        };
+        dockerls = {
+          enable = true;
+          filetypes = [
+            "dockerfile"
+          ];
+        };
+        docker_compose_language_service = {
+          enable = true;
+          filetypes = [
+            "yaml"
+            "yml"
+          ];
+          settings = {
+            dockerCompose = {
+              schema = "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json";
+              validate = true;
+              lint = {
+                deprecatedProperties = true;
+                duplicateKeys = true;
+              };
+            };
+          };
+        };
+        yamlls = {
+          enable = true;
+          settings = {
+            yaml = {
+              schemas = {
+                "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" =
+                  "docker-compose*.yml";
+              };
+              validate = true;
+              format.enable = true;
+              completion = true;
+              hover = true;
+            };
+          };
+        };
       };
     };
     nix = {
@@ -17,21 +140,16 @@
     };
     lsp-signature = {
       enable = true;
-      settings = {
-        hint_prefix = "😼 ";
-      };
+      settings.hint_prefix = "😼 ";
     };
     comment = {
       enable = true;
       settings = {
-        toggler = {
-          line = "<C-/>";
-        };
+        toggler.line = "<C-/>";
         opleader.line = "<C-/>";
       };
     };
   };
-
   keymaps = [
     {
       mode = "n";
