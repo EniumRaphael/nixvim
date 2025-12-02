58b71076d5ca6947449908c86e286609438126de
Raphael <rparodi@student.42.fr>
2025-12-02 13:49:19 +0100

    fix(lsp/asm): deprecated option used removed

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -25,10 +25,11 @@
           cmd = [
             "asm-lsp"
           ];
-          on_attach = ''
-            vim.env.ASM_LSP_CONFIG = vim.fn.getcwd() .. "/asm-lsp.toml"
-          '';
-        };
+          onAttach = {
+            function = ''
+              vim.env.ASM_LSP_CONFIG = vim.fn.getcwd() .. "/asm-lsp.toml"
+            '';
+          };        };
         clangd = {
           enable = true;
           cmd = [
